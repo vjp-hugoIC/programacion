4c1aceb3e8d72d754bff4b95b08e6b61457ccba4
vjp-hugoIC <hizquierdoc03@informatica.iesvalledeljerteplasencia.es>
2025-10-27 18:02:47 +0100

    locura

---
+++ tema 4/ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio2;
+import java.util.Scanner;
+import utilidades.utilidades;
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner entrada = new Scanner(System.in);
+         
+         System.out.println("Por favor, introduzca un numero: ");
+         double num1 = scanner.nextInt();
+        
+        // TODO code application logic here
+    }
+    
+}
