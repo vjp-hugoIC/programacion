ffd9b4e409b5060f4a910942e2f44d4de4acd732
vjp-hugoIC <hizquierdoc03@informatica.iesvalledeljerteplasencia.es>
2025-11-10 17:56:51 +0100

    bicicleta

--- tema 5/ejemplo1/src/ejemplo1/Ejemplo1.java
+++ tema 5/ejemplo1/src/ejemplo1/Ejemplo1.java
@@ -31,15 +31,50 @@ public class Ejemplo1 {
         
         //1.2 Con un constructor parametrizado, inicializando los atributos 
         //con los valores pasados por parametro
-        Coche tuCoche = new Coche("Michelin", "Bajo", 3);
-        System.out.println(tuCoche.toString());
+        //Coche tuCoche = new Coche("Michelin", "Bajo", 3);
+        //System.out.println(tuCoche.toString());
         
         //Esto hace lo mismo que la linea anterior
-        System.out.println(tuCoche);
+        //System.out.println(tuCoche);
         
-        System.out.println(tuCoche.getRuedas());
+        //System.out.println(tuCoche.getRuedas());
         
-        // TODO code application logic here
+        
+        //MODIFICADORES DE ACCESO
+        //Bicicleta bicicleta = new Bicicleta();
+        //System.out.println(bicicleta.ruedas);
+        //System.out.println(bicicleta.getEstaPedaleando());
+        
+        //System.out.println("Cuantas bicis hemos creado");
+        
+        
+        //Bicicleta bicicleta2 = new Bicicleta();
+        //System.out.println("Cuantas bicis hemos creado");
+        //System.out.println(bicicleta.getNumBicicletas());
+        
+        //PASO DE PARAMETROS POR VALOR 0 POR PREFERENCIA
+        int num1 = 4;
+        System.out.println(suma(num1));
+        
+        Bicicleta bici = new Bicicleta();
+        empezarAPedalear(bici);
+        
     }
     
-}
+    
+    
+    
+        public static int suma(int num1) {
+        int num2 = 3;
+        return num1 + num2;
+        }
+        
+        public static void empezarAPedalear(Bicicleta bici) {
+            bici.setEstaPedaleando(true);
+        }
+        
+        // TODO code application logic here
+        
+    }
+    
+
