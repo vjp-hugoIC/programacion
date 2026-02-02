12d98468434767f8d5c697c25f0040535808ad44
vjp-hugoIC <hizquierdoc03@informatica.iesvalledeljerteplasencia.es>
2026-02-02 18:04:52 +0100

    3 y 4

---
+++ desdoble/ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // declaro las variables
+        int edad = 21;
+        float altura = 1.81f;
+        
+        //salida de los datos
+        System.out.println("Mi nombre es Hugo, tengo " + edad + " años y mido " + altura);
+        
+        // TODO code application logic here
+    }
+    
+}
---
+++ desdoble/ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio4;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int nota1 = 6;
+        int nota2 = 9;
+        int notaMedia = (nota1 + nota2) / 2;
+        
+        System.out.println("Programacion:" + "La nota del primer examen es: " + nota1 + ", la nota del segundo examen es " + nota2 + " y la media es: " + notaMedia);
+        
+        // TODO code application logic here
+    }
+    
+}
