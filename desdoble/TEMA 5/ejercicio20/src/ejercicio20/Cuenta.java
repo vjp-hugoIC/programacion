902543d7d71c050c11162e65e152f12f4ef7c7d5
vjp-hugoIC <hizquierdoc03@informatica.iesvalledeljerteplasencia.es>
2026-04-07 15:27:23 +0200

    okokf5o

--- desdoble/TEMA 5/ejercicio20/src/ejercicio20/Cuenta.java
+++ desdoble/TEMA 5/ejercicio20/src/ejercicio20/Cuenta.java
@@ -10,4 +10,56 @@ package ejercicio20;
  */
 public class Cuenta {
     
+    //Atributos
+    private double saldo;
+    
+    //Constructores 
+    //Por defecto
+    public Cuenta() {
+    
+        saldo = 0.0;
+    }
+    
+    //Parametrizado
+    public Cuenta(int saldo) {
+    
+        this.saldo = saldo;
+    }
+    
+    //Gettes y setters
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    //Meodo mostrar
+    public void mostrarCuenta(){
+    
+        System.out.println("Saldo: " + saldo);
+    }
+    
+    //ToString
+
+    @Override
+    public String toString() {
+        return "Cuenta{" + "saldo=" + saldo + '}';
+    }
+    
+    //Otros metodos
+    //Metodo ingresar
+     public void ingresar(double num) {
+
+         saldo = saldo + num;
+     }
+     
+     //Metodo extraer
+     public void extraer(double num) {
+     
+         saldo = saldo - num;
+     }
+    
+    
 }
--- desdoble/TEMA 5/ejercicio20/src/ejercicio20/Ejercicio20.java
+++ desdoble/TEMA 5/ejercicio20/src/ejercicio20/Ejercicio20.java
@@ -4,16 +4,88 @@
  */
 package ejercicio20;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
  */
 public class Ejercicio20 {
+    
+    //Metodo para mostrar el menu
+    public static void mostrarMenu() {
+    
+        System.out.println("1. Crear cuenta vacia");
+        System.out.println("2. Crear cuenta con saldo inicial");
+        System.out.println("3. Ingresar dinero");
+        System.out.println("4. Sacar dinero");
+        System.out.println("5. Ver saldo");
+        System.out.println("6. Salir");
+    }
+    
+    //Metodo para pedir la opcion del menu al usuario
+    public static int pedirOpcion() {
+    
+        int num;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Selecciona la opcion que quieras realizar: ");
+        num = entrada.nextInt();
+        
+        return num;
+    }
+    
+    //Metodo para pedir el dinero inicial de la cuenta en case de pulsar la opcion 2
+    public static int saldoInicial() {
+    
+        int num;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Introduce el saldo inicial de la cuenta: ");
+        num = entrada.nextInt();
+        
+        return num;
+    } 
+    
+    //Metodo para pedir el ingreso de dinero 
+    public static int dineroIngreso() {
+    
+        int num;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Introduce el dinero que quieres ingresar en la cuenta: ");
+        num = entrada.nextInt();
+        
+        return num;
+    } 
+    
+    //Opciones del menu
+    public static void opciones (int num) {
+        
+        Cuenta cuenta1;
+    
+        switch (num) {
+    
+            case 1:
+                cuenta1 = new Cuenta();
+                break;
+            case 2: 
+                cuenta1 = new Cuenta(saldoInicial());
+                break;
+            case 3: 
+                cuenta1.ingresar(dineroIngreso());
+        }    
+    }
+    
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
+        
         // TODO code application logic here
     }
     
