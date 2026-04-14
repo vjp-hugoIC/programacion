613803c788bd393529ce1015ceaba99d1d547500
vjp-hugoIC <hizquierdoc03@informatica.iesvalledeljerteplasencia.es>
2026-04-14 18:44:10 +0200

    ingeorgne

--- Tema 12/ejercicio5/src/ejercicio5/Ejercicio5.java
+++ Tema 12/ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -18,118 +18,135 @@ import java.io.PrintWriter;
  * @author alumno
  */
 public class Ejercicio5 {
-    
+
     public final static String AGENDA = "agenda.txt";
-    
+
     //Metodo para mostrar las opciones del menu
-    public static void opciones(){
-    
+    public static void opciones() {
+
         System.out.println("1. Añadir un contacto a un fichero de texto");
         System.out.println("2. Mostrar por pantalla el contenido del fichero de texto creado");
         System.out.println("3. Salir del programa");
     }
-    
+
     //pedir opciones del menu
     public static int pedirOpcion() {
-    
+
         System.out.println("Elige una opcion");
-        
+
         int opcion;
         Scanner entrada = new Scanner(System.in);
         opcion = entrada.nextInt();
-        
+
         return opcion;
     }
-    
+
     //Metodo para pedir nombre
     public static String pedirNombre() throws InputMismatchException {
-    
+
         String nombre;
         Scanner entrada = new Scanner(System.in);
-        
+
         System.out.println("Nombre: ");
         nombre = entrada.nextLine();
         return nombre;
     }
-    
-     //Metodo para pedir edad
+
+    //Metodo para pedir edad
     public static int pedirEdad() {
-    
+
         int edad;
         Scanner entrada = new Scanner(System.in);
-        
+
         System.out.println("Edad: ");
         edad = entrada.nextInt();
         return edad;
     }
-    
-     //Metodo para pedir numero de telefono
+
+    //Metodo para pedir numero de telefono
     public static int pedirTelf() {
-    
+
         int telf;
         Scanner entrada = new Scanner(System.in);
-        
+
         System.out.println("Telefono: ");
         telf = entrada.nextInt();
         return telf;
     }
-    
-    
+
     //Metodo para escribir en el fichero
     public static void escribirFichero(String fichero, String nombre, int edad, int telefono) throws FileNotFoundException, IOException {
         System.out.println("ESCRIBIENDO EN EL FICHERO");
-        
-        try(
-                FileWriter fw = new FileWriter(nombre);
-                PrintWriter pw = new PrintWriter(fw);
-                ) {
-         pw.println(nombre);
-         pw.println(edad);
-         pw.println(telefono);
-            
+
+        try (
+                FileWriter fw = new FileWriter(nombre); PrintWriter pw = new PrintWriter(fw);) {
+            pw.println(nombre);
+            pw.println(edad);
+            pw.println(telefono);
+
         }
-        
+
     }
-    
+
     //Metodo para leer el fichero
     public static void leerFichero(String fichero) throws FileNotFoundException, IOException {
         System.out.println("LEYENDO FICHERO");
-        
+
         try (
-                FileReader fr = new FileReader(fichero);
-                BufferedReader br = new BufferedReader(fr);
-                ) {
-        
+                FileReader fr = new FileReader(fichero); BufferedReader br = new BufferedReader(fr);) {
+
             String linea = br.readLine();
-            
-            while(linea != null) {
-                
-                    System.out.println(linea);
-                    linea = br.readLine();
-                }
+
+            while (linea != null) {
+
+                System.out.println(linea);
+                linea = br.readLine();
+            }
         }
-        
+
     }
-    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
+        do {
         //Muestro las opciones del menu al usuario
         opciones();
-        
-        switch(pedirOpcion()) {
-        
-            case 1: 
-                try {
-                escribirFichero(AGENDA, pedirNombre(), pedirEdad(), pedirTelf());
-                }
+
+        try {
+            switch (pedirOpcion()) {
+
+                 case 1:
+
+                    escribirFichero(AGENDA, pedirNombre(), pedirEdad(), pedirTelf());
+                    break;
+                    
+                 case 2:
+                     leerFichero(AGENDA);
+                     break;
+                     
+                 case 3: 
+                     break;
+                     
+                 default: 
+                     System.out.println("Elige una opcion correcta");
+
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("ERROR: Has escrito un tipo de dato incorrecto.");
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: Archivo no encontrado.");
+        } catch (IOException e) {
+            System.out.println("ERROR: Ha ocurrido un error en la escritura o lectura del fichero.");
+        } catch (Exception e) {
+            System.out.println("ERROR: Ha ocurrido un error desconocido.");
         }
         
         
+        } while(pedirOpcion() >= 2);
         // TODO code application logic here
     }
-    
+
 }
